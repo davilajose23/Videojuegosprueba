903b03fc334e94d493226b753022269fbf422b28
Jose Fernando <davila.jose23@gmail.com>
2015-02-18 11:28:30 -0600

    Agregue la clase JuegoApplet

---
+++ src/JuegoApplet.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JoseFernando
+ * 18/feb/2015
+ */
+public class JuegoApplet {
+    
+    
+}
